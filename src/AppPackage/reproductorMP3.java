b02e9e52ce57c578358cd05507a0b3b842859ed9
Eisner Lopez <elopeza@ucatolica.ac.cr>
2016-08-06 01:14:55 -0600

    Agrega Ventana acerca de los autores

--- src/AppPackage/reproductorMP3.java
+++ src/AppPackage/reproductorMP3.java
@@ -1,29 +1,26 @@
 package AppPackage;
 
 //All code is added to create the widget. Follow the "Create a Widget" tutorial if you want to know how I did it.
-
 import java.awt.Toolkit;
 import java.io.File;
-import java.io.FileFilter;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class reproductorMP3 extends javax.swing.JDialog 
-{
-    MainClass mc  = new MainClass();
-    
+public class reproductorMP3 extends javax.swing.JDialog {
+
+    MainClass mc = new MainClass();
+
     int xMouse;
     int yMouse;
-    
+
     int width = (Toolkit.getDefaultToolkit().getScreenSize().width / 2) - 185;
     int height = Toolkit.getDefaultToolkit().getScreenSize().height - 180;
-    
-    public reproductorMP3(java.awt.Frame parent, boolean modal) 
-    {
+
+    public reproductorMP3(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
-        
-        this.setLocation(width, height);
+
+        this.setLocationRelativeTo(null);
     }
 
     @SuppressWarnings("unchecked")
@@ -39,9 +36,16 @@ public class reproductorMP3 extends javax.swing.JDialog
         Background = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("MP3 Player");
+        setLocation(new java.awt.Point(640, 480));
+        setMaximumSize(new java.awt.Dimension(370, 220));
         setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -111,13 +115,40 @@ public class reproductorMP3 extends javax.swing.JDialog
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 130, 370, 90));
 
+        jMenu1.setText("File");
+
+        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_X, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem1.setText("Salir");
+        jMenuItem1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jMenuItem1MouseReleased(evt);
+            }
+        });
+        jMenu1.add(jMenuItem1);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Ayuda");
+
+        jMenuItem2.setText("Acerca");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void BackgroundMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BackgroundMouseDragged
         int x = evt.getXOnScreen();
         int y = evt.getYOnScreen();
-        
+
         this.setLocation(x - xMouse, y - yMouse);
     }//GEN-LAST:event_BackgroundMouseDragged
 
@@ -152,32 +183,49 @@ public class reproductorMP3 extends javax.swing.JDialog
     private void SelectFileMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SelectFileMouseReleased
         // TODO add your handling code here:
         FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos MP3", "mp3", "mpeg3");
-        
+
         JFileChooser chooser = new JFileChooser("C:\\Musica");
         chooser.addChoosableFileFilter(filter);
-        
+
         int returnVal = chooser.showOpenDialog(null);
-        
-        if (returnVal == JFileChooser.APPROVE_OPTION)
-        {
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
             mc.Stop();
-            
+
             File myFile = chooser.getSelectedFile();
             String song = myFile + "";
-            
+
             String name = chooser.getSelectedFile().getName();
             Display.setText(name);
-            
-            
+
             mc.Play(song);
         }
     }//GEN-LAST:event_SelectFileMouseReleased
 
-    public static void main(String args[]) 
-    {
+    private void jMenuItem1MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuItem1MouseReleased
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_jMenuItem1MouseReleased
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+        // TODO add your handling code here:
+        About dialog = new About(new javax.swing.JFrame(), true);
+        dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        dialog.setVisible(true);
+
+
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    public static void main(String args[]) {
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -198,16 +246,12 @@ public class reproductorMP3 extends javax.swing.JDialog
         //</editor-fold>
         //</editor-fold>
 
-        java.awt.EventQueue.invokeLater(new Runnable() 
-        {
-            public void run() 
-            {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
                 reproductorMP3 dialog = new reproductorMP3(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() 
-                {
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) 
-                    {
+                    public void windowClosing(java.awt.event.WindowEvent e) {
                         System.exit(0);
                     }
                 });
@@ -225,6 +269,11 @@ public class reproductorMP3 extends javax.swing.JDialog
     private javax.swing.JLabel SelectFile;
     private javax.swing.JLabel Stop;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
